4a5424328f29a33c117675ef1e463229c6659981
JonnyPugh <jpugh@umich.edu>
2017-10-09 01:58:45 -0400

    Changed Stats to not get posts from API The Facebook Graph API has a bug currently with getting all posts on pages using their API (https://developers.facebook.com/bugs/1838195226492053/), so I'm changing the approach of the Stats interface to take a list of Facebook post ids. Going forward I will continue to save ids of automatic posts to avoid depending on the Graph API to get them.

--- stats/__init__.py
+++ stats/__init__.py
@@ -1,20 +1,14 @@
 from requests import Session
 
-# Get all posts on a page
-# Get the information from those posts using a worker thread/Queue design
+# Get the information from specified posts using a worker thread/Queue design
 # Provide an interface to get useful statistics about the posts
 
 class Stats(object):
-	def __init__(self, access_token):
+	def __init__(self, access_token, post_ids):
 		self.__api_url = "https://graph.facebook.com/v2.10/"
 		self.__session = Session()
 		self.__session.params = {"access_token": access_token}
-
-		posts = self.__request(self.__api_url + "me/posts")
-		self.__posts = posts["data"]
-		while "next" in posts["paging"]:
-			posts = self.__request(posts["paging"]["next"])
-			self.__posts.extend(posts["data"])
+		self.__post_ids = post_ids
 
 	def __request(self, path):
 		r = self.__session.get(path)
